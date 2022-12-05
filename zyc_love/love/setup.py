a23c117f96965680644d8a365815fe3cc8eeba03
renaissancezyc <118403818+renaissancezyc@users.noreply.github.com>
2022-12-05 20:12:15 +0800

    Add files via upload

---
+++ zyc_love/love/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="zyc_love",
+    version="0.0.1",
+    author="Renaissance",
+    author_email="Renaissance3310@qq.com",
+    description=" Code of Love ",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+
+)
+
+
+
+
+
